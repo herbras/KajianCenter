3e9f4db55d778b710be7c118c65fa3532855f6b7
Sarbeh <handoyonoterakhir@gmail.com>
2023-12-08 00:31:42 +0700

    init: Set up project configuration and package management files

---
+++ astro.config.mjs
@@ -0,0 +1,9 @@
+import { defineConfig } from 'astro/config';
+
+import svelte from "@astrojs/svelte";
+import UnoCSS from "unocss/astro";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [svelte(),UnoCSS()]
+});
