731abe55cefeefa6aa6e6f9303cf0a3653036b2f
Sarbeh <handoyonoterakhir@gmail.com>
2023-12-08 02:27:35 +0700

    feat: Integrate Cloudflare and Moment js in Astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,18 @@
 import { defineConfig } from 'astro/config';
-
 import svelte from "@astrojs/svelte";
 import UnoCSS from "unocss/astro";
 
+import cloudflare from "@astrojs/cloudflare";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [svelte(),UnoCSS()]
+  integrations: [svelte(), UnoCSS()],
+  output: "server",  vite: {
+    build: {
+      rollupOptions: {
+        external: ["moment"]
+      }
+    }
+  },
+  adapter: cloudflare()
 });
