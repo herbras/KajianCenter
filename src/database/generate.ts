16a6446ba6572af13d7ef8ac5c95b44874d0e142
Sarbeh <handoyonoterakhir@gmail.com>
2023-12-08 02:24:15 +0700

    feat: Extend generate.ts with additional enums, interfaces, and exports

--- src/database/generate.ts
+++ src/database/generate.ts
@@ -1,48 +1,63 @@
 // Enum untuk Kategori Ilmu
 enum KategoriIlmu {
-    Fiqh = "Fiqh",
-    Aqidah = "Aqidah",
-    Adab = "Adab",
-  }
-  
-  // Enum untuk Jenis Kajian
-  enum JenisKajian {
-    Tematik = "Tematik",
-    Rutin = "Rutin",
-  }
-  
-  interface Alamat {
-    jalan?: string; // Nama jalan, opsional
-    kabupaten: string;
-    provinsi: string;
-    kodepos: number;
-  }
-  
-  // Interface untuk Contact Person
-  interface ContactPerson {
-    nama: string;
-    telepon: string; // Nomor telepon contact person
-    email?: string; // Email contact person, opsional
-  }
-  
-  
-  // Interface untuk Kajian
-  interface Kajian {
-    judul: string;
-    tempat: string; // Nama masjid atau lokasi kajian
-    alamat?: Alamat;
-    hariJadwal: string; // Harus disimpan dalam format yang konsisten, misal ISO 8601 untuk tanggal
-    linkGoogleMaps: string; // URL ke Google Maps
-    gambarKajian?: string; // URL ke gambar kajian, opsional
-    deskripsi: string;
-    contactPerson: ContactPerson;
-    jenisKajian?: JenisKajian; // Opsional untuk MVP, diperlukan untuk fitur lanjutan
-    kategoriIlmu?: KategoriIlmu[]; // Array, opsional untuk MVP, diperlukan untuk fitur lanjutan
-  }
-  
-  import { faker } from '@faker-js/faker';
- export const allKategoriIlmu = Object.values(KategoriIlmu);
-  export const allJenisKajian = Object.values(JenisKajian);
+  Fiqh = "Fiqh",
+  Aqidah = "Aqidah",
+  Adab = "Adab",
+} 
+
+enum StatusBiayaKajian {
+  Berbayar = "Berbayar",
+  Gratis = "Gratis"
+}
+
+
+// Enum untuk Jenis Kajian
+enum JenisKajian {
+  Tematik = "Tematik",
+  Rutin = "Rutin",
+}
+// Interface untuk Ustadz/Pemateri
+interface Pemateri {
+  nama: string;
+  deskripsiSingkat: string; // Deskripsi singkat tentang ustadz/pemateri
+  foto?: string; // URL ke foto ustadz/pemateri, opsional
+  kontak?: string; // Kontak ustadz/pemateri, bisa berupa nomor telepon atau email
+}
+interface Alamat {
+  jalan?: string; // Nama jalan, opsional
+  kabupaten: string;
+  provinsi: string;
+  kodepos: number;
+}
+
+// Interface untuk Contact Person
+interface ContactPerson {
+  nama: string;
+  telepon: string; // Nomor telepon contact person
+  email?: string; // Email contact person, opsional
+}
+
+
+// Interface untuk Kajian
+interface Kajian {
+  judul: string;
+  tempat: string; // Nama masjid atau lokasi kajian
+  alamat?: Alamat;
+  hariJadwal: string; // Harus disimpan dalam format yang konsisten, misal ISO 8601 untuk tanggal
+  linkGoogleMaps: string; // URL ke Google Maps
+  gambarKajian?: string; // URL ke gambar kajian, opsional
+  deskripsi: string;
+  contactPerson: ContactPerson;
+  pemateri: Pemateri; // Menambahkan pemateri ke dalam Kajian
+  jenisKajian?: JenisKajian; // Opsional untuk MVP, diperlukan untuk fitur lanjutan
+  kategoriIlmu?: KategoriIlmu[]; // Array, opsional untuk MVP, diperlukan untuk fitur lanjutan
+  statusBiaya?: StatusBiayaKajian;
+}
+
+import { faker } from '@faker-js/faker';
+export const allKategoriIlmu = Object.values(KategoriIlmu);
+export const allJenisKajian = Object.values(JenisKajian);
+export const allJenisStatus = Object.values(StatusBiayaKajian);
 // Mengenerate satu entri data kajian
 function generateKajian(): Kajian {
   return {
@@ -54,6 +69,12 @@ function generateKajian(): Kajian {
       provinsi: faker.location.state(),
       kodepos: parseInt(faker.location.zipCode()),
     },
+    pemateri: {
+      nama: faker.person.fullName(),
+      deskripsiSingkat: faker.lorem.paragraph(),
+      foto: faker.image.url(),
+      kontak: faker.word.noun(),
+    },
     hariJadwal: faker.date.soon().toISOString(),
     linkGoogleMaps: `https://maps.google.com/?q=${faker.location.latitude()},${faker.location.longitude()}`,
     gambarKajian: faker.image.url(),
@@ -64,8 +85,9 @@ function generateKajian(): Kajian {
       email: faker.internet.email(),
     },
     jenisKajian: faker.helpers.arrayElement(allJenisKajian),
-    kategoriIlmu: faker.helpers.arrayElements(allKategoriIlmu,  faker.number.int({ min: 1, max: 1 }) ),
-    };
+    statusBiaya: faker.helpers.arrayElement(allJenisStatus),
+    kategoriIlmu: faker.helpers.arrayElements(allKategoriIlmu, faker.number.int({ min: 1, max: 1 })),
+  };
 }
 
 export const dummyKajians: Kajian[] = Array.from({ length: 12 }, generateKajian);
