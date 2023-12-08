9b21c8abd44300572186bb2a69167847ccb13955
Sarbeh <handoyonoterakhir@gmail.com>
2023-12-08 22:31:33 +0700

    feat: remove cloudflare and server mode

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,17 +2,14 @@ import { defineConfig } from 'astro/config';
 import svelte from "@astrojs/svelte";
 import UnoCSS from "unocss/astro";
 
-import cloudflare from "@astrojs/cloudflare";
-
 // https://astro.build/config
 export default defineConfig({
   integrations: [svelte(), UnoCSS()],
-  output: "server",  vite: {
+   vite: {
     build: {
       rollupOptions: {
         external: ["moment"]
       }
     }
   },
-  adapter: cloudflare()
 });
